24d08fca60bba64ba70f7a15234b72795870b6bd
Charitha Saumya <cgusthin@purdue.edu>
2023-04-03 23:22:02 -0400

    updates

--- libosip/run-cfm-driver.sh
+++ libosip/run-cfm-driver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ulimit -s unlimited
+
 python3 ${KLEE_BUILD_DIR}/../scripts/cfm_driver/driver.py -i test-driver.bc -k driver_options.json
 
 echo "Done" | mail -s "libosip run complete!" $USER
