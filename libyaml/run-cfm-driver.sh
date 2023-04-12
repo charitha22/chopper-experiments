c638d12b1979514270c5d1a68d8a66a31d1ba75f
Charitha Saumya <cgusthin@purdue.edu>
2023-04-11 21:59:28 -0400

    run files

--- libyaml/run-cfm-driver.sh
+++ libyaml/run-cfm-driver.sh
@@ -3,6 +3,6 @@
 ulimit -s unlimited
 
 mkdir -p /local/scratch/a/$USER/libyaml
-python3 ${KLEE_BUILD_DIR}/../scripts/cfm_driver/driver.py -i test-driver.bc -k driver_options.json -r /local/scratch/a/$USER/libyaml
+python3 ${KLEE_BUILD_DIR}/../scripts/cfm_driver/driver.py -e -i test-driver.bc -k driver_options.json -r /local/scratch/a/$USER/libyaml
 
 echo "Done" | mail -s "libyaml run complete!" $USER
